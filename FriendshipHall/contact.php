247218cf47f97a357ef1ccb972d36a3d2427b175
Diego Quiros <2314diego@gmail.com>
2020-01-12 13:04:51 -0800

    1

--- FriendshipHall/contact.php
+++ FriendshipHall/contact.php
@@ -1,9 +1,20 @@
-
-Thanks for your message!
-
 <?php
-	$to = "2314diego@gmail.com";
-	$subject = "Email from my website";
-	$body = "the user typed in: " . $_POST['myEmail'];
-	mail($to, $subject, $body);
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $msg = $_POST['message'];
+
+        $to = "2314diego@gmail.com";
+        $subject = 'Form Submission';
+        $message="Name: ".$name."\n." ."Phone: ".$phone. "\n". "Wrote the following: "."\n\n"/$msg;
+        $headers="From: ".$email;
+
+        if mail($mailTo, $subject, $txt, $headers)){
+            echo "<h1>Sent Successfully! Thank you"."".$name.",We will contact you shortly!</h1>";
+        }
+        else{
+            echo "Something went wrong!";
+        }
+
+    }
 ?>
