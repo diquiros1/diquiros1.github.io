482cd5e495329e9c5861ad15065b6639ea7ba4c9
Diego Quiros <2314diego@gmail.com>
2020-01-12 11:43:35 -0800

    Update contact.php

--- FriendshipHall/contact.php
+++ FriendshipHall/contact.php
@@ -12,6 +12,6 @@ $headers = "From: ".$mailFrom;
 $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
 mail($mailTo, $subject, $txt, $headers);
-header("Location: contact.php?mailsend");
+header("Location: contact.php");
 }
     
