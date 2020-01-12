cd9be2153e7bee948d578d4123d7e578912f87ed
Diego Quiros <2314diego@gmail.com>
2020-01-12 11:29:58 -0800

    1

--- FriendshipHall/contact.php
+++ FriendshipHall/contact.php
@@ -1,16 +1,27 @@
 <?php
    
-if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
+    $mailFrom = $_POST['email'];
     $message = $_POST['message'];
+
+    $email_from = '2314diego@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
      
-    $mailTO = "2314diego@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    $to = "2314diego@gmail.com";
+    $headers = "From: $mailFrom; /r/n";
+    $headers .= "Reply-To: $visitor_email /r/n";
+    $txt = "You have received an e-mail from ".$name.".\r\n".$message;
     
-    mail($mailTO, $subject, $txt, $headers);
-    header("Location: contact.php?mailsend");
-    }
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html");
+    
+
+?>
     
